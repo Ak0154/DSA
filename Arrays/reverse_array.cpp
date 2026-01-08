67cffb4be42c2ffa1397755d571f8da09083b8bc
AK <akashkumar200405@gmail.com>
2026-01-08 19:47:36 +0530

    reverse an array

---
+++ Arrays/reverse_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int array[5]={1,2,3,4,5};
+    int n= sizeof(array)/sizeof(array[0]);
+    cout<<n<<endl;
+    cout<<"Reversed array is: "<<endl;
+    for(int i=n-1;i>=0;i--){
+        cout<<array[i]<<" ";
+    }
+
+
+    return 0;
+}
